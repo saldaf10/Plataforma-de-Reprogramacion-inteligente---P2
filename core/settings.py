332dcc39733df9537fb243a4b60d00ec31ed9e48
saldaf10 <psaldarrif@eafit.edu.co>
2025-11-05 13:31:50 -0500

    Fix: Use CompressedStaticFilesStorage instead of Manifest for WhiteNoise

--- core/settings.py
+++ core/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # WhiteNoise configuration for serving static files in production
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
